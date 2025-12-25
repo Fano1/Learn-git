9906df3d9814bdae6af37b5981eee1de4c7789ae
sudip pandey <sudippandey774@gmail.com>
2025-12-25 16:06:26 +0545

    goru ayush

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Hello sudip")
+a=100
+b=200
+c=a+b
+print("Sum is:",c)
+print("Sum of",a,"and",b,"is",c)
+print(f"Sum of {a} and {b} is {c}")
+print("Sum of {} and {} is {}".format(a,b,c))
+print("Hello sudip")
