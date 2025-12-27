4b02d06bcd3796d3179f602145b4edbebd00bbd7
Fano1 <ayushyush100@gmail.com>
2025-12-27 22:41:25 +0545

    bruh

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ import cv2 as cv
 
 def main():
     st.write("Fuck the free world")
-
+    st.form()
+    st.form_submit_button()
 
 def openCamera():
     print("cameara")
+
+def lookj():
+    pass
+
+def see():
+    pass
